fc5bc6ae2ec53f9ac864364a75f4327e1b26715a
Jeremy <jeremy.butin@yahoo.com>
2021-06-04 17:46:39 +0200

    appareil photo

--- views/AppareilPhoto.tsx
+++ views/AppareilPhoto.tsx
@@ -1,46 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet,TouchableOpacity , AppRegistry} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, AppRegistry } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
 const PendingView = () => (
     <View
-      style={{
-        flex: 1,
-        backgroundColor: 'lightgreen',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+        style={{
+            flex: 1,
+            backgroundColor: 'lightgreen',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}
     >
-      <Text>Waiting</Text>
+        <Text>Waiting</Text>
     </View>
-  );
+);
 
 const AppareilPhotos = ({ navigation }) => {
     return (
+        
         <View style={styles.container}>
-        <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-        >
-          {({ camera, status, recordAudioPermissionStatus }) => {
-            if (status !== 'READY') return <PendingView />;
-            return (
-              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
-              </View>
-            );
-          }}
-        </RNCamera>
-      </View>
+            <RNCamera
+                style={styles.preview}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                androidCameraPermissionOptions={{
+                    title: 'Permission to use camera',
+                    message: 'We need your permission to use your camera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                    title: 'Permission to use audio recording',
+                    message: 'We need your permission to use your audio',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancel',
+                }}
+            >
+
+                {({ camera, status, recordAudioPermissionStatus }) => {
+                    if (status !== 'READY') return <PendingView />;
+                    return (
+                        <> 
+                            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+                                    <Text style={{ fontSize: 15 }}> SNAP </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </>
+                    );
+                }}
+            </RNCamera>
+        </View>
     );
 }
 
@@ -49,28 +59,39 @@ async function takePicture(camera) {
     const data = await camera.takePictureAsync(options);
     //  eslint-disable-next-line
     console.log(data.uri);
-  };
+};
 
 export default AppareilPhotos;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black',
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
     },
     preview: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'center',
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
     },
     capture: {
-      flex: 0,
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      padding: 15,
-      paddingHorizontal: 20,
-      alignSelf: 'center',
-      margin: 20,
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+        marginBottom: 120
     },
-  });
+    flash: {
+        flex: 0,
+        alignContent: 'flex-end',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
+});
